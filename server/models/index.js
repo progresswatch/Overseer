e7f9558e58775c904ab5b9c720b216aea17eb76a
Admin <admin@Admins-MacBook-Pro.local>
2017-02-13 14:48:19 -0800

    set up models

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,17 @@ const { Sequelize, sequelize } = db;
 const Models = {};
 
 Models.User = require('./user');
+Models.Project = require('./project');
+Models.Task = require('./task');
 
-sequelize.sync();
+Models.Project.belongsTo(Models.User);
+Models.User.hasMany(Models.Project);
+
+Models.Task.belongsTo(Models.Project);
+Models.Project.hasMany(Models.Task);
+
+sequelize.sync({
+    // force:true
+});
 
 module.exports = Models;
