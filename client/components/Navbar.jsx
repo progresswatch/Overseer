b024649a5e7e47ed190d2886aa85f8c9b243c01d
Joseph Vu <jvu009@ucr.edu>
2017-02-13 16:57:38 -0800

    add styling

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { IndexLink, Link } from 'react-router';
+
+const Navbar = () => (
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <IndexLink className="navbar-brand" to="/">ProgressWatch</IndexLink>
+      </div>
+
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul className="nav navbar-nav navbar-right">
+          <li><Link to='signup'>Sign up</Link></li>
+          <li><Link to='login'>Login</Link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
